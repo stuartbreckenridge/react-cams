d9c743c047dc267b798e3412446b781427c89dc4
Stuart Breckenridge <stuartbreckenridge@users.noreply.github.com>
2025-10-26 20:32:26 +0800

    Create react-cams.js

---
+++ react-cams.js
@@ -0,0 +1,625 @@
+var minimal = `<svg id="webcamShape" viewBox="0 0 969 49.9" style="enable-background:new 0 0 969 49.9;" xml:space="preserve">
+<g id="dashes-bottom_00000102512600782541945230000004087363406915768995_">
+	<g>
+		<g>
+			<g>
+				<polygon class="st0" points="612.7,14.2 644.3,14.2 652.9,22.7 621.3,22.7 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="675.9,14.2 707.5,14.2 716.1,22.7 684.5,22.7 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="739.1,14.2 770.7,14.2 779.3,22.7 747.7,22.7 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="802.3,14.2 833.9,14.2 842.5,22.7 810.9,22.7 				"/>
+			</g>
+		</g>
+	</g>
+	<g>
+		<g>
+			<g>
+				<polygon class="st0" points="353,14.2 321.4,14.2 312.9,22.7 344.5,22.7 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="289.8,14.2 258.2,14.2 249.7,22.7 281.3,22.7 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="226.6,14.2 195,14.2 186.5,22.7 218.1,22.7 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="163.4,14.2 131.8,14.2 123.3,22.7 154.9,22.7 				"/>
+			</g>
+		</g>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="323.3,48.9 131.4,48.9 120.7,38.2 334.3,37.4 		"/>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="839.5,48.9 644.4,48.9 633.6,38.2 850.5,37.4 		"/>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="329.7,23.6 348.5,4.3 617.4,4.3 637,23.9 		"/>
+	</g>
+</g>
+<polygon class="st2" points="949.5,22.9 621.7,22.9 613.2,14.3 353.2,14.3 344.7,22.9 19.6,22.9 3.6,38.9 351.3,38.9 359.8,30.3 
+	606.5,30.3 615.1,38.9 965.5,38.9 "/>
+<g>
+	<polygon class="st1" points="11.6,30.8 1.6,40.8 11.6,40.8 139.3,40.8 129.3,30.8 	"/>
+</g>
+<g>
+	<polygon class="st1" points="957.5,30.8 967.5,40.8 957.5,40.8 829.8,40.8 839.8,30.8 	"/>
+</g>
+</svg>
+`
+
+var square = `<svg id="webcamShape" viewBox="0 0 744.5 747.9" style="enable-background:new 0 0 744.5 747.9;" xml:space="preserve">
+<g id="dashes-top_00000036216635462312739680000002854507122716077503_">
+	<g>
+		<g>
+			<polygon class="st0" points="242.7,24.5 211.1,24.5 202.6,16 234.2,16 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="179.5,24.5 147.9,24.5 139.3,16 171,16 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="116.3,24.5 84.7,24.5 76.1,16 107.7,16 			"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<polygon class="st0" points="502.4,24.5 534,24.5 542.5,16 510.9,16 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="565.6,24.5 597.2,24.5 605.7,16 574.1,16 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="628.8,24.5 660.4,24.5 668.9,16 637.3,16 			"/>
+		</g>
+	</g>
+</g>
+<g id="dashes-bottom_00000169516267494359095660000017897621524944245649_">
+	<g>
+		<g>
+			<polygon class="st0" points="501.9,711.4 533.5,711.4 542.1,719.9 510.5,719.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="565.1,711.4 596.7,711.4 605.3,719.9 573.7,719.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="628.3,711.4 659.9,711.4 668.5,719.9 636.9,719.9 			"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<polygon class="st0" points="242.3,711.4 210.7,711.4 202.1,719.9 233.7,719.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="179.1,711.4 147.5,711.4 138.9,719.9 170.5,719.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="115.9,711.4 84.3,711.4 75.7,719.9 107.3,719.9 			"/>
+		</g>
+	</g>
+</g>
+<g>
+	<polygon class="st1" points="506.8,34.5 238.6,34.5 219,14.9 525.6,15.1 	"/>
+</g>
+<g>
+	<polygon class="st1" points="212.6,746.1 128.6,746.1 117.9,735.3 223.6,734.5 	"/>
+</g>
+<g>
+	<polygon class="st1" points="618.8,746.1 533.6,746.1 522.9,735.3 629.8,734.5 	"/>
+</g>
+<g>
+	<polygon class="st1" points="218.9,720.8 237.8,701.5 506.7,701.5 526.3,721 	"/>
+</g>
+<g>
+	<path class="st2" d="M744.5,736H504.3l-8.5-8.5H249.1l-8.5,8.5H0V0h240.9l8.5,8.5h246.4l8.5-8.5h240.2V736z M511,720h217.5V16H511
+		l-8.5,8.5H242.8l-8.5-8.5H16v704h217.9l8.5-8.5h260L511,720z"/>
+</g>
+<defs>
+	<filter id="Adobe_OpacityMaskFilter" filterUnits="userSpaceOnUse" x="0" y="0" width="744.5" height="736">
+		<feFlood  style="flood-color:white;flood-opacity:1" result="back"/>
+		<feBlend  in="SourceGraphic" in2="back" mode="normal"/>
+	</filter>
+</defs>
+<mask maskUnits="userSpaceOnUse" x="0" y="0" width="744.5" height="736" id="SVGID_1_">
+	<g class="st3">
+		<g>
+			<rect x="-54.8" y="-58.9" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -151.2285 370.9022)" width="853.7" height="853.7"/>
+		</g>
+	</g>
+</mask>
+<g class="st4">
+	<path class="st1" d="M736.5,8v720H8V8H736.5 M744.5,0h-8H8H0v8v720v8h8h728.5h8v-8V8V0L744.5,0z"/>
+</g>
+</svg>
+`
+
+var portrait = `<svg id="webcamShape" viewBox="0 0 963.5 746.1">
+<g id="dashes-top_00000078755331252431576890000004817535536910996125_">
+	<g>
+		<g>
+			<g>
+				<polygon class="st0" points="350.7,24.5 319.1,24.5 310.6,16 342.2,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="287.5,24.5 255.9,24.5 247.3,16 279,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="224.3,24.5 192.7,24.5 184.1,16 215.7,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="161.1,24.5 129.5,24.5 120.9,16 152.5,16 				"/>
+			</g>
+		</g>
+	</g>
+	<g>
+		<g>
+			<g>
+				<polygon class="st0" points="610.4,24.5 642,24.5 650.5,16 618.9,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="673.6,24.5 705.2,24.5 713.7,16 682.1,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="736.8,24.5 768.4,24.5 776.9,16 745.3,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="800,24.5 831.6,24.5 840.1,16 808.5,16 				"/>
+			</g>
+		</g>
+	</g>
+</g>
+<g id="dashes-bottom_00000057853449886096309740000009146904719118624648_">
+	<g>
+		<g>
+			<g>
+				<polygon class="st0" points="609.9,711.4 641.5,711.4 650.1,719.9 618.5,719.9 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="673.1,711.4 704.7,711.4 713.3,719.9 681.7,719.9 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="736.3,711.4 767.9,711.4 776.5,719.9 744.9,719.9 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="799.5,711.4 831.1,711.4 839.7,719.9 808.1,719.9 				"/>
+			</g>
+		</g>
+	</g>
+	<g>
+		<g>
+			<g>
+				<polygon class="st0" points="350.3,711.4 318.7,711.4 310.1,719.9 341.7,719.9 				"/>
+			</g>
+		</g>
+		<g>
+
+			<g>
+				<polygon class="st0" points="287.1,711.4 255.5,711.4 246.9,719.9 278.5,719.9 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="223.9,711.4 192.3,711.4 183.7,719.9 215.3,719.9 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="160.7,711.4 129.1,711.4 120.5,719.9 152.1,719.9 				"/>
+			</g>
+		</g>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="614.8,34.5 346.6,34.5 327,14.9 633.6,15.1 		"/>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="320.6,746.1 128.6,746.1 117.9,735.3 331.6,734.5 		"/>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="836.8,746.1 641.6,746.1 630.9,735.3 847.8,734.5 		"/>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="326.9,720.8 345.8,701.5 614.7,701.5 634.3,721 		"/>
+	</g>
+</g>
+<g>
+	<g>
+		<path class="st2" d="M963.5,736H612.3l-8.5-8.5H357.1l-8.5,8.5H0V0h348.9l8.5,8.5h246.4l8.5-8.5h351.2V736z M619,720h328.5V16H619
+			l-8.5,8.5H350.8l-8.5-8.5H16v704h325.9l8.5-8.5h260L619,720z"/>
+	</g>
+</g>
+<defs>
+	<filter id="Adobe_OpacityMaskFilter" filterUnits="userSpaceOnUse" x="0" y="0" width="963.5" height="736">
+		<feFlood  style="flood-color:white;flood-opacity:1" result="back"/>
+		<feBlend  in="SourceGraphic" in2="back" mode="normal"/>
+	</filter>
+</defs>
+<mask maskUnits="userSpaceOnUse" x="0" y="0" width="963.5" height="736" id="SVGID_1_">
+	<g class="st3">
+		<g>
+			
+				<rect x="-26.4" y="-138.5" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -119.596 447.2697)" width="1013.1" height="1013.1"/>
+		</g>
+	</g>
+</mask>
+<g class="st4">
+	<path class="st1" d="M955.5,8v720H8V8H955.5 M963.5,0h-8H8H0v8v720v8h8h947.5h8v-8V8V0L963.5,0z"/>
+</g>
+</svg>`
+
+var portraitRotated = `<svg id="webcamShape" viewBox="0 0 760 963.5" style="enable-background:new 0 0 760 963.5;" xml:space="preserve">
+<g id="dashes-top_00000129884178526789048650000004523000331740025747_">
+	<g>
+		<g>
+			<polygon class="st0" points="723.5,350.7 723.5,319.1 732,310.6 732,342.2 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,287.5 723.5,255.9 732,247.3 732,279 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,224.3 723.5,192.7 732,184.1 732,215.7 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,161.1 723.5,129.5 732,120.9 732,152.5 			"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<polygon class="st0" points="723.5,610.4 723.5,642 732,650.5 732,618.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,673.6 723.5,705.2 732,713.7 732,682.1 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,736.8 723.5,768.4 732,776.9 732,745.3 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,800 723.5,831.6 732,840.1 732,808.5 			"/>
+		</g>
+	</g>
+</g>
+<g id="dashes-bottom_00000026132296394514797280000009648776203104204988_">
+	<g>
+		<g>
+			<polygon class="st0" points="36.6,609.9 36.6,641.5 28.1,650.1 28.1,618.5 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,673.1 36.6,704.7 28.1,713.3 28.1,681.7 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,736.3 36.6,767.9 28.1,776.5 28.1,744.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,799.5 36.6,831.1 28.1,839.7 28.1,808.1 			"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<polygon class="st0" points="36.6,350.3 36.6,318.7 28.1,310.1 28.1,341.7 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,287.1 36.6,255.5 28.1,246.9 28.1,278.5 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,223.9 36.6,192.3 28.1,183.7 28.1,215.3 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,160.7 36.6,129.1 28.1,120.5 28.1,152.1 			"/>
+		</g>
+	</g>
+</g>
+<g>
+	<polygon class="st1" points="732.9,633.6 713.5,614.8 713.5,346.6 733.1,327 	"/>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="13.5,330.6 1.9,319.6 1.9,127.6 12.7,116.9 		"/>
+	</g>
+	<g>
+		<polygon class="st1" points="13.3,844 1.9,832.6 1.9,642.6 12.7,631.9 		"/>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="747.3,843.6 746.5,631 758,641.9 758,832.9 		"/>
+	</g>
+	<g>
+		<polygon class="st1" points="747.3,330.7 746.7,116.2 758,127.7 758,319.9 		"/>
+	</g>
+</g>
+<g>
+	<polygon class="st1" points="27,634.3 27.2,326.9 46.5,345.8 46.5,614.7 	"/>
+</g>
+<g>
+	<path class="st2" d="M748,963.5H12V612.3l8.5-8.5V357.1l-8.5-8.5V0h736v348.9l-8.5,8.5v246.4l8.5,8.5V963.5z M28,947.5h704V619
+		l-8.5-8.5V350.8l8.5-8.5V16H28v325.9l8.5,8.5v260L28,619V947.5z"/>
+</g>
+<defs>
+	<filter id="Adobe_OpacityMaskFilter" filterUnits="userSpaceOnUse" x="12" y="0" width="736" height="963.5">
+		<feFlood  style="flood-color:white;flood-opacity:1" result="back"/>
+		<feBlend  in="SourceGraphic" in2="back" mode="normal"/>
+	</filter>
+</defs>
+<mask maskUnits="userSpaceOnUse" x="12" y="0" width="736" height="963.5" id="SVGID_1_">
+	<g class="st3">
+		<g>
+			
+				<rect x="-126.5" y="-26.4" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -228.1856 409.3199)" width="1013.1" height="1013.1"/>
+		</g>
+	</g>
+</mask>
+<g class="st4">
+	<path class="st1" d="M740,8v947.5H20V8H740 M748,0h-8H20h-8v8v947.5v8h8h720h8v-8V8V0L748,0z"/>
+</g>
+</svg>`
+
+var standard = `<svg id="webcamShape" viewBox="0 0 1296 746.1" style="enable-background:new 0 0 1296 746.1;" xml:space="preserve">
+<g id="dashes-top">
+	<g>
+		<g>
+			<g>
+				<polygon class="st0" points="518.6,24.5 487,24.5 478.4,16 510,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="455.4,24.5 423.8,24.5 415.2,16 446.8,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="392.2,24.5 360.6,24.5 352,16 383.6,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="329,24.5 297.4,24.5 288.8,16 320.4,16 				"/>
+			</g>
+		</g>
+	</g>
+	<g>
+		<g>
+			<g>
+				<polygon class="st0" points="778.3,24.5 809.9,24.5 818.4,16 786.8,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="841.5,24.5 873.1,24.5 881.6,16 850,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="904.7,24.5 936.3,24.5 944.8,16 913.2,16 				"/>
+			</g>
+		</g>
+		<g>
+			<g>
+				<polygon class="st0" points="967.9,24.5 999.5,24.5 1008,16 976.4,16 				"/>
+			</g>
+		</g>
+	</g>
+</g>
+<g id="dashes-bottom">
+	<g>
+		<g>
+			<polygon class="st0" points="777.8,711.4 809.4,711.4 818,719.9 786.4,719.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="841,711.4 872.6,711.4 881.2,719.9 849.6,719.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="904.2,711.4 935.8,711.4 944.4,719.9 912.8,719.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="967.4,711.4 999,711.4 1007.6,719.9 976,719.9 			"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<polygon class="st0" points="518.2,711.4 486.6,711.4 478,719.9 509.6,719.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="455,711.4 423.4,711.4 414.8,719.9 446.4,719.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="391.8,711.4 360.2,711.4 351.6,719.9 383.2,719.9 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="328.6,711.4 297,711.4 288.4,719.9 320,719.9 			"/>
+		</g>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="782.7,34.5 514.5,34.5 494.9,14.9 801.5,15.1 		"/>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="488.5,746.1 128.6,746.1 117.9,735.3 499.4,734.5 		"/>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="1169.3,746.1 809.5,746.1 798.7,735.3 1180.3,734.5 		"/>
+	</g>
+</g>
+<g>
+	<g>
+		<polygon class="st1" points="494.8,720.8 513.6,701.5 782.6,701.5 802.2,721 		"/>
+	</g>
+</g>
+<g>
+	<g>
+		<g>
+			<path class="st2" d="M1296,736H780.2l-8.5-8.5H525l-8.5,8.5H0V0h516.8l8.5,8.5h246.4l8.5-8.5H1296V736z M786.8,720H1280V16H786.8
+				l-8.5,8.5H518.7l-8.5-8.5H16v704h493.8l8.5-8.5h260L786.8,720z"/>
+		</g>
+	</g>
+</g>
+<defs>
+	<filter id="Adobe_OpacityMaskFilter" filterUnits="userSpaceOnUse" x="0" y="0" width="1296" height="736">
+		<feFlood  style="flood-color:white;flood-opacity:1" result="back"/>
+		<feBlend  in="SourceGraphic" in2="back" mode="normal"/>
+	</filter>
+</defs>
+<mask maskUnits="userSpaceOnUse" x="0" y="0" width="1296" height="736" id="SVGID_1_">
+	<g class="st3">
+		<g>
+			<rect x="23.3" y="-256.7" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -70.4205 565.9899)" width="1249.4" height="1249.4"/>
+		</g>
+	</g>
+</mask>
+<g class="st4">
+	<path class="st1" d="M1288,8v720H8V8H1288 M1296,0h-8H8H0v8v720v8h8h1280h8v-8V8V0L1296,0z"/>
+</g>
+</svg>`
+var standardRotated = `<svg id="webcamShape" viewBox="0 0 760 1296" style="enable-background:new 0 0 760 1296;" xml:space="preserve">
+<g id="dashes-top_00000128484137175756126480000008207697694270331800_">
+	<g>
+		<g>
+			<polygon class="st0" points="723.5,518.6 723.5,487 732,478.4 732,510 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,455.4 723.5,423.8 732,415.2 732,446.8 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,392.2 723.5,360.6 732,352 732,383.6 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,329 723.5,297.4 732,288.8 732,320.4 			"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<polygon class="st0" points="723.5,778.3 723.5,809.9 732,818.4 732,786.8 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,841.5 723.5,873.1 732,881.6 732,850 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,904.7 723.5,936.3 732,944.8 732,913.2 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="723.5,967.9 723.5,999.5 732,1008 732,976.4 			"/>
+		</g>
+	</g>
+</g>
+<g id="dashes-bottom_00000044178248234816509690000006528745510807920775_">
+	<g>
+		<g>
+			<polygon class="st0" points="36.6,777.8 36.6,809.4 28.1,818 28.1,786.4 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,841 36.6,872.6 28.1,881.2 28.1,849.6 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,904.2 36.6,935.8 28.1,944.4 28.1,912.8 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,967.4 36.6,999 28.1,1007.6 28.1,976 			"/>
+		</g>
+	</g>
+	<g>
+		<g>
+			<polygon class="st0" points="36.6,518.2 36.6,486.6 28.1,478 28.1,509.6 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,455 36.6,423.4 28.1,414.8 28.1,446.4 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,391.8 36.6,360.2 28.1,351.6 28.1,383.2 			"/>
+		</g>
+		<g>
+			<polygon class="st0" points="36.6,328.6 36.6,297 28.1,288.4 28.1,320 			"/>
+		</g>
+	</g>
+</g>
+<g>
+	<polygon class="st1" points="732.9,801.5 713.5,782.7 713.5,514.5 733.1,494.9 	"/>
+</g>
+<g>
+	<polygon class="st1" points="13.5,498.4 1.9,487.5 1.9,128.6 12.7,117.9 	"/>
+</g>
+<g>
+	<polygon class="st1" points="13.3,1178.9 1.9,1167.6 1.9,809.5 12.7,798.7 	"/>
+</g>
+<g>
+	<polygon class="st1" points="746.5,498.4 747.3,117.9 758,128.6 758,487.5 	"/>
+</g>
+<g>
+	<polygon class="st1" points="746.6,1178.9 747.3,798.7 758,809.5 758,1167.6 	"/>
+</g>
+<g>
+	<polygon class="st1" points="27,802.2 27.2,494.8 46.5,513.6 46.5,782.6 	"/>
+</g>
+<g>
+	<path class="st2" d="M748,1296H12V780.2l8.5-8.5V525l-8.5-8.5V0h736v516.8l-8.5,8.5v246.4l8.5,8.5V1296z M28,1280h704V786.8
+		l-8.5-8.5V518.7l8.5-8.5V16H28v493.8l8.5,8.5v260l-8.5,8.5V1280z"/>
+</g>
+<defs>
+	<filter id="Adobe_OpacityMaskFilter" filterUnits="userSpaceOnUse" x="12" y="0" width="736" height="1296">
+		<feFlood  style="flood-color:white;flood-opacity:1" result="back"/>
+		<feBlend  in="SourceGraphic" in2="back" mode="normal"/>
+	</filter>
+</defs>
+<mask maskUnits="userSpaceOnUse" x="12" y="0" width="736" height="1296" id="SVGID_1_">
+	<g class="st3">
+		<g>
+			
+				<rect x="-244.7" y="23.3" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -346.9058 458.4954)" width="1249.4" height="1249.4"/>
+		</g>
+	</g>
+</mask>
+<g class="st4">
+	<path class="st1" d="M740,8v1280H20V8H740 M748,0h-8H20h-8v8v1280v8h8h720h8v-8V8V0L748,0z"/>
+</g>
+</svg>`
